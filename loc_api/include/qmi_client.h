23ddcf22500d254b1a3e92594e1b05a2a6dc41a4
Pavel Dubrova <pashadubrova@gmail.com>
2019-12-19 07:10:28 +0200

    squashme
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- loc_api/include/qmi_client.h
+++ loc_api/include/qmi_client.h
@@ -4,14 +4,76 @@
 #include "qmi_idl_lib.h"
 
 typedef int qmi_client_error_type;
+typedef unsigned int qmi_service_instance;
 
-qmi_client_error_type (*qmi_client_notifier_init) ();
-qmi_client_error_type (*qmi_client_init) ();
-qmi_client_error_type (*qmi_client_send_msg_sync) ();
-qmi_client_error_type (*qmi_client_release) ();
-qmi_client_error_type (*qmi_client_message_decode) ();
-qmi_client_error_type (*qmi_client_get_any_service) ();
-qmi_client_error_type (*qmi_client_get_service_instance) ();
-qmi_client_error_type (*qmi_client_register_error_cb) ();
+typedef void (*qmi_client_ind_cb) (
+    qmi_client_type                 user_handle,
+    unsigned int                    msg_id,
+    void                            *ind_buf,
+    unsigned int                    ind_buf_len,
+    void                            *ind_cb_data
+);
+
+typedef void (*qmi_client_error_cb) (
+    qmi_client_type                 user_handle,
+    qmi_client_error_type           error,
+    void                            *err_cb_data
+);
+
+qmi_client_error_type (*qmi_client_notifier_init) (
+    qmi_idl_service_object_type     service_obj,
+    qmi_client_os_params            *os_params,
+    qmi_client_type                 *user_handle
+);
+
+qmi_client_error_type (*qmi_client_init) (
+    qmi_service_info                *service_info,
+    qmi_idl_service_object_type     service_obj,
+    qmi_client_ind_cb               ind_cb,
+    void                            *ind_cb_data,
+    qmi_client_os_params            *os_params,
+    qmi_client_type                 *user_handle
+);
+
+qmi_client_error_type (*qmi_client_send_msg_sync) (
+    qmi_client_type                 user_handle,
+    unsigned int                    msg_id,
+    void                            *req_c_struct,
+    unsigned int                    req_c_struct_len,
+    void                            *resp_c_struct,
+    unsigned int                    resp_c_struct_len,
+    unsigned int                    timeout_msecs
+);
+
+qmi_client_error_type (*qmi_client_release) (
+    qmi_client_type     user_handle
+);
+
+qmi_client_error_type (*qmi_client_message_decode) (
+    qmi_client_type                 user_handle,
+    qmi_idl_type_of_message_type    req_resp_ind,
+    unsigned int                    message_id,
+    const void                      *p_src,
+    unsigned int                    src_len,
+    void                            *p_dst,
+    unsigned int                    dst_len
+);
+
+qmi_client_error_type (*qmi_client_get_any_service) (
+    qmi_idl_service_object_type     service_obj,
+    qmi_service_info                *service_info
+);
+
+qmi_client_error_type (*qmi_client_get_service_instance) (
+    qmi_idl_service_object_type     service_obj,
+    qmi_service_instance            instance_id,
+    qmi_service_info                *service_info
+);
+
+qmi_client_error_type (*qmi_client_register_error_cb) (
+    qmi_client_type                 user_handle,
+    qmi_client_error_cb             err_cb,
+    void                            *err_cb_data
+);
 
 #endif /* QMI_CLIENT_H */
